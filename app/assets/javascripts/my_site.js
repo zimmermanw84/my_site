70b317ab0ce2b0c371b74c243c721f7e8bda664a
Walter Zimmerman <zimmermanw84@gmail.com>
2015-04-18 12:37:50 -0700

    backbone and boostrap env setup

---
+++ app/assets/javascripts/my_site.js
@@ -0,0 +1,13 @@
+window.MySite = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    // alert('Hello from Backbone!');
+  }
+};
+
+$(function(){
+  MySite.initialize();
+});

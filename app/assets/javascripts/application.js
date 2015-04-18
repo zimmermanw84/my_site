a4793ef05a135ab4d026f23f40c6cc541b3946c4
Walter Zimmerman <zimmermanw84@gmail.com>
2015-04-18 12:56:29 -0700

    .keep added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,8 @@
 //= require underscore
 //= require backbone
 //= require my_site
-
+//= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
-//= require_tree ./routers
 //= require_tree .

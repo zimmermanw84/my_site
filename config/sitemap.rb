333cde5a9d14b340000cdbf8f007036d94696933
Walter Zimmerman <zimmermanw84@gmail.com>
2015-05-11 15:26:13 -0700

    DSN update with sitemap fix

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.saltman.space"
+SitemapGenerator::Sitemap.default_host = "http://www.waltzimmerman.com"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.

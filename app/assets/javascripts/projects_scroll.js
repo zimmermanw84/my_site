eda73d6b83b602d3ca3e1041f3c246ea8b35dc61
Walter Zimmerman <zimmermanw84@gmail.com>
2015-05-12 18:54:18 -0700

    timeout animation added

---
+++ app/assets/javascripts/projects_scroll.js
@@ -0,0 +1,30 @@
+(function() {
+
+  $project = $('#pro').height() + 500
+  // 867 window index
+
+  var projectsDisplay = function() {
+    $(window).scroll(function() {
+
+      if ( $(this).scrollTop() > $project ) {
+
+        setTimeout(function() {
+          $('#smart-potato-main').addClass('animated slideInRight').show()
+          $('#salty-main').addClass('animated slideInLeft').show()
+        }, 500)
+
+        setTimeout(function() {
+          $('#see-need-main').addClass('animated slideInRight').show();
+          $('#frack-jack-main').addClass('animated slideInLeft').show()
+        }, 1500)
+
+      }
+    })
+  };
+
+  $(function() {
+    $(".project-container").hide()
+    projectsDisplay();
+  });
+
+})();
